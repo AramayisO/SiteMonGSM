596ade4dda3e91bb4972d837e6dd98eeb771c9e3
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2019-12-13 01:45:30 -0800

    Allow debug macro to take format string and variable number of arguments

--- include/debug.h
+++ include/debug.h
@@ -1,14 +1,21 @@
+/**
+ * @file debug.h
+ * @brief This module provides convenience functions and macros for debugging.
+ * @author Aramayis Orkusyan
+ * @date December 8, 2019
+ * @compyright GNU General Public License v3.0
+ */
 #ifndef SITE_MON_GSM_DEBUG_H
 #define SITE_MON_GSM_DEBUG_H
 
 #include <stdio.h>
 
 #ifndef NDEBUG
-    #define DEBUG_LOG(stream, str) { \
-        fprintf(stream, str); \
+    #define DEBUG_LOG(stream, str, ...) { \
+        fprintf(stream, str, ##__VA_ARGS__); \
     }
 #else
-    #define DEBUG_LOG(stream, str) { \
+    #define DEBUG_LOG(stream, str, ...) { \
     }
 #endif // NDEBUG
 
